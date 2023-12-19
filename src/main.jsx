5d828f82ee42f45ba1936e3b2aaa8e4ae43e03bf
Your Name <you@example.com>
2023-12-19 14:59:56 -0300

    git

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter basename="/">
+  <HashRouter>
     <React.StrictMode>
       <Routes>
         <Route path="*" element={<App />} />
